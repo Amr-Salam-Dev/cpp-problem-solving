c0cc7a0e1019d7206c5f31185b7cd62c3070881e
Amr-Salam-Dev <mohamdamr8@gmail.com>
2026-05-13 01:20:53 +0300

    Add files via upload

---
+++ P1.cpp
@@ -0,0 +1,90 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+	//حل مسئله رقم 7
+	//int num1 = 10, num2 = 20, num3 = 30;
+	//cout << "num1: " << num1 << endl;
+	//cout << "num2: " << num2 << endl;
+	//cout << "num3: " << num3 << endl;
+	//int sum = num1 + num2 + num3;
+	//cout << "The sum of num1, num2 and num3 is: " << sum << endl;
+	// //////////////////////////////////
+	//حل مسئله رقم 9
+	//int a = 90, b = 80, c = 70;
+	//int sum = (a + b + c) / 3;
+	//cout << "the sum Or a,b,c is =" << sum << endl;
+	///////////////////////////////////////
+	//حل مسئله رقم 10
+	//int num1 = 10, num2 = 20;
+	//cout << "num1: " << num1 << endl;
+	//cout << "num2: " << num2 << endl;
+	//cout << "اكبر رقم : " << max(num1, num2) << endl; // 20
+	////////////////////////////////
+	//حل مسئله رقم 14 
+	//int a=5, b=8;
+	//int arya = a * sqrt(b * b - a * a);
+	//cout << arya<< endl;
+	//////////////////////////////////
+	// حل مسئله رقم 15
+	//int a = 20, b = 10;
+	//cout << "مساحت مستطیل  : " << a * b << endl;
+	////////////////////////////////
+	//حل مسئله رقم 17
+	//int a = 10, h = 8;
+	//cout << "مساحت مثلث : " << 0.5* a * h << endl;//40
+	////////////////////////////////
+//حل مسئله رقم 19
+	//int d;
+	//cout << "enter the diameter of the circle : ";
+	//cin >> d;
+	//double pi = 3.14; // double تنفع مع الكسور
+	//double area = (pi * d * d) / 4;
+	//cout << "The area of the circle is: " << area << endl;//78.5
+	/////////////////////////////
+	//حل مسئله رقم 20
+	//int A;
+	//cout << "enter the area of the circle : ";
+	//cin >> A;
+	//double pi = 3.14;
+	//double Area = pi * A * A / 4;
+	//cout << "The area of the circle is: " << Area << endl;//78.5
+	////////////////////////////////
+	//حل مسئله رقم 21
+	// مساحه الدائره ن طريق المحيط
+	//int L;//20
+	//cout << "enter the area of the Circle : " << endl;
+	//cin >> L;
+	//double pi = 3.14;
+	//double Area = (L * L) / (4 * pi);
+	//cout << "The area of the circle is: " << Area << endl;//31.8
+	/////////////////////////////////
+	//حل مسئله رقم 22
+	//int a, b;
+	//cout << "Enter A her : " << endl;
+	//cin >> a;
+	//cout << "Enter B her : " << endl;
+	//cin >> b;
+	//double pi = 3.14;
+	//double Area = (pi * (b * b) / 4.0) * ((2.0 * a - b) / (2.0 * a + b));
+	//cout << "The Area is :  " << Area << endl;//47.124
+	////////////////////////////////
+	//حل مسئله رقم 31
+	int Num;
+	cout << "Enter the Num : " << endl;
+	cin >> Num;
+	int a = pow(Num, 2);// pow بتستخدم بدل الاس العشري ونضيف قيمة الأس في الاخر
+	int b = pow(Num, 3);
+	int c = pow(Num, 4);
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;// 9 //27 //81
+
+
+    return 0;
+}
+
